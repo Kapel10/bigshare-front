3cd42ae8b2c4fc84ab23bd360ed9ab614d7fc46b
rakhymzhan <Rahimzhan_T@mail.ru>
2024-09-05 21:12:05 +0500

    tailwind settings

--- bigshare-front/src/main.tsx
+++ bigshare-front/src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <>
+            home page!
+        </>
+    },
+    {
+        path: '/sign-in',
+        element: <>
+            sign in page!
+        </>
+    }
+])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router}/>
+    </StrictMode>,
 )
